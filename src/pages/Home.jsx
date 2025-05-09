d734bd661412b4da4d576e12a2680209e4ab0d9e
7S-Codere <dylan.e33@hotmail.fr>
2025-05-09 19:28:55 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import Chat from '../components/chat/Chat'; 
 import VideoPlayer from '../components/stream/VideoPlayer';
 import '../css/pages/Home.scss';
-import Navbar from '../components/Navbar'; // Assurez-vous que le chemin est correct
 
 const Home = () => {
   return (
     <main>
-     <Navbar />
       <div className="box">
         <VideoPlayer />
         <Chat />
