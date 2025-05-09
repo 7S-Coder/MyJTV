2e48fd2ac5a0694456dd2a7444d2bd065b5aab42
7S-Codere <dylan.e33@hotmail.fr>
2025-05-09 18:55:31 +0200

    update

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';import React from 'react';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Navbar;};  );    </nav>      </div>        <h1>My Navbar</h1>      <div className="navbar-content">    >      }}        width: '100%',        top: 0,        position: 'fixed',        zIndex: 9999,        backgroundColor: 'red',      style={{      className="navbar"    <nav  return (const Navbar: React.FC = () => {
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      {/* Add your navbar content here */}
+    </nav>
+  );
+};
+
+export default Navbar;
