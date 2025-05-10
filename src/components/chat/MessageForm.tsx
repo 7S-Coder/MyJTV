13127dca90a84012874070fb2e853260243fe30c
7S-Codere <dylan.e33@hotmail.fr>
2025-05-10 21:29:10 +0200

    update

--- src/components/chat/MessageForm.tsx
+++ src/components/chat/MessageForm.tsx
@@ -75,18 +75,6 @@ const MessageForm: React.FC<MessageFormProps> = ({
     };
   }, []);
 
-  useEffect(() => {
-    const setVh = () => {
-        const vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
-
-    window.addEventListener('resize', setVh);
-    setVh(); // Initial call
-
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
-
   const handleInputChange = (value: string) => {
     setNewMessage(value);
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.jsx'
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App.jsx';
+
+const Root = () => {
+  useEffect(() => {
+    const handleResize = () => {
+      const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+      if (isIOS) {
+        document.querySelector('.box')?.style.setProperty('height', `${window.innerHeight}px`);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Appel initial
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return <App />;
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);
